adb9ea852a4547242f4b491a6e40cb2cc377f52a
TechWiz-3 <programmingplus1@gmail.com>
2022-07-22 12:03:48 +1000

    📦 NEW: add boolean testing

--- action.py
+++ action.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+# this makes the script executable - not required
 
 from os import getenv
 
+# importing the inputs defined in action.yml and the workflow file
 var_a = getenv("INPUT_VAR1")
 unrequired_input = getenv("INPUT_VAR2")
+bool_handling = getenv("INPUT_IM_A_BOOL")
 
 print("Ayo")
 print(f"Variable 1 is {var_a}")
 print(f"Unrequired and unentered variable: {unrequired_input}")
+print(f"This was entered as 'true' in the workflow file: {bool_handling}")
